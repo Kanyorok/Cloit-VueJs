6498a3dbe11c2253b014918738ef68db1fa22eb8
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-05-17 19:46:32 +0300

    Structure the components to have a good flow desktop view

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,18 +1,28 @@
 <template>
   <q-page class="bg-secondary">
-    <div class="row bg-primary">
+    <div class="row bg-primary header-view">
       <div class="col">
         <div class="main-section">
           <h1>Looking for foreign talent with top capabilities?</h1>
-          <p>
+          <p class="main-section-paragraph">
             With Hyperhire, you can quickly find the best foreign talent. Meet
             top-tier talents instantly by clicking below.
           </p>
           <q-btn-group flat>
-            <q-btn label="Visa Talent" />
-            <q-btn label="Permanent Residence" />
-            <q-btn label="F-4 (Overseas Koreans)" />
-            <q-btn label="General" />
+            <div class="border-top-white">
+              <span>Visa Talent</span>
+              <p>Wages are calculated based on your country.</p>
+            </div>
+            <div class="border-top-white">
+              <span>Permanent Residence</span>
+              <p>Don't worry if you don't fit the job when you hire them.</p>
+            </div>
+            <div class="border-top-white">
+              <span>F-4 (Overseas Koreans)</span>
+              <p>
+                Even if you need someone urgently, quick hiring is possible.
+              </p>
+            </div>
           </q-btn-group>
         </div>
       </div>
@@ -24,7 +34,21 @@
 </template>
 
 <style scoped>
+.border-top-white {
+  border-top: 2px solid #fff;
+  border-width: 3px;
+  margin-right: 15px;
+}
+.border-top-white p {
+  font-family: Robot;
+  font-weight: 400;
+  font-size: 16px;
+}
+.aligned-left {
+  text-align: left;
+}
 .main-section {
+  padding-top: 40px;
   text-align: Left;
   color: #fff;
 }
@@ -33,6 +57,15 @@
   font-weight: 900;
   font-size: 48px;
   line-height: 62.4px;
+}
+.main-section-paragraph {
+  font-family: Poppins;
+  font-weight: 900;
+  font-size: 24px;
+  line-height: 34px;
+}
+.header-view {
+  padding: 0px 100px 0 100px;
 }
 </style>
 
