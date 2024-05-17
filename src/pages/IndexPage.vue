5d29cac6a5ad248a2bff306a8b351dd20351c71c
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-05-17 18:30:37 +0300

    Replicate the layout to achieve the set design

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,31 +1,23 @@
 <template>
-  <q-page padding>
-    <div class="main-section">
-      <h1>Looking for foreign talent with top capabilities?</h1>
-      <p>
-        With Hyperhire, you can quickly find the best foreign talent. Meet
-        top-tier talents instantly by clicking below.
-      </p>
-      <q-btn-group flat>
-        <q-btn label="Visa Talent" />
-        <q-btn label="Permanent Residence" />
-        <q-btn label="F-4 (Overseas Koreans)" />
-        <q-btn label="General" />
-      </q-btn-group>
-      <div class="talent-card">
-        <q-card>
-          <q-card-section>
-            <div class="q-gutter-sm">
-              <div>Abhishek Gupta</div>
-              <div>Nationality: Indian</div>
-              <div>Major: Computer Science</div>
-              <div>
-                Introduction: Graduate from ABC University with 5 years of
-                experience in software development.
-              </div>
-            </div>
-          </q-card-section>
-        </q-card>
+  <q-page class="bg-secondary">
+    <div class="row bg-primary">
+      <div class="col">
+        <div class="main-section">
+          <h1>Looking for foreign talent with top capabilities?</h1>
+          <p>
+            With Hyperhire, you can quickly find the best foreign talent. Meet
+            top-tier talents instantly by clicking below.
+          </p>
+          <q-btn-group flat>
+            <q-btn label="Visa Talent" />
+            <q-btn label="Permanent Residence" />
+            <q-btn label="F-4 (Overseas Koreans)" />
+            <q-btn label="General" />
+          </q-btn-group>
+        </div>
+      </div>
+      <div class="col">
+        <SliderLink />
       </div>
     </div>
   </q-page>
@@ -33,16 +25,19 @@
 
 <style scoped>
 .main-section {
-  text-align: center;
+  text-align: Left;
+  color: #fff;
 }
-.talent-card {
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
+.main-section h1 {
+  font-family: Poppins;
+  font-weight: 900;
+  font-size: 48px;
+  line-height: 62.4px;
 }
 </style>
 
 <script setup>
+import SliderLink from "components/SliderLink.vue";
 defineOptions({
   name: "IndexPage",
 });
