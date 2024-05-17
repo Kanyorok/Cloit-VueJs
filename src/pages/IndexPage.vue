203821a14722905314c37bd022f8258ab1c8a35f
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-05-17 21:10:48 +0300

    Add the responsiveness of the page to the app

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -34,13 +34,14 @@
 </template>
 
 <style scoped>
+/* Default styles */
 .border-top-white {
   border-top: 2px solid #fff;
   border-width: 3px;
   margin-right: 15px;
 }
 .border-top-white p {
-  font-family: Robot;
+  font-family: Roboto;
   font-weight: 400;
   font-size: 16px;
 }
@@ -49,7 +50,7 @@
 }
 .main-section {
   padding-top: 40px;
-  text-align: Left;
+  text-align: left;
   color: #fff;
 }
 .main-section h1 {
@@ -65,7 +66,95 @@
   line-height: 34px;
 }
 .header-view {
-  padding: 0px 100px 0 100px;
+  padding: 0px 100px;
+}
+
+/* Styles for mobile devices */
+@media (max-width: 767px) {
+  .main-section {
+    padding-top: 20px;
+    text-align: left;
+    color: #fff;
+  }
+  .header-view {
+    display: flex;
+    flex-direction: column;
+  }
+  .swiper-button-next {
+    color: #fff !important;
+  }
+  .q-btn-group {
+    display: none;
+  }
+  .main-section h1 {
+    font-size: 32px;
+    line-height: 42px;
+  }
+  .main-section-paragraph {
+    font-size: 18px;
+    line-height: 28px;
+  }
+  .header-view {
+    padding: 0px 20px;
+  }
+  .border-top-white {
+    margin-right: 10px;
+  }
+  .border-top-white p {
+    font-size: 14px;
+  }
+}
+
+/* Styles for tablets and larger screens */
+@media (min-width: 768px) {
+  .main-section {
+    padding-top: 40px;
+    text-align: left;
+    color: #fff;
+  }
+  .main-section h1 {
+    font-size: 48px;
+    line-height: 62.4px;
+  }
+  .main-section-paragraph {
+    font-size: 24px;
+    line-height: 34px;
+  }
+  .header-view {
+    padding: 0px 100px;
+  }
+  .border-top-white {
+    margin-right: 15px;
+  }
+  .border-top-white p {
+    font-size: 16px;
+  }
+}
+
+/* Styles for larger desktop screens */
+@media (min-width: 1024px) {
+  .main-section {
+    padding-top: 60px;
+    text-align: left;
+    color: #fff;
+  }
+  .main-section h1 {
+    font-size: 56px;
+    line-height: 70px;
+  }
+  .main-section-paragraph {
+    font-size: 28px;
+    line-height: 38px;
+  }
+  .header-view {
+    padding: 0px 120px;
+  }
+  .border-top-white {
+    margin-right: 20px;
+  }
+  .border-top-white p {
+    font-size: 18px;
+  }
 }
 </style>
 
