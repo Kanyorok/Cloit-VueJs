483fcb304968b77d8573727ee22e8b4a6a480c92
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-05-17 18:29:17 +0300

    Add the component design guidelines

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,8 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-img
-            src="../static/logo/hyperhire.png"
-            style="width: 114px"
-          ></q-img>
-        </q-toolbar-title>
+      <q-toolbar class="custom-toolbar">
+        <q-img src="../static/logo/hyperhire.png" style="width: 114px"></q-img>
         <div class="gt-sm">
           <q-btn
             flat
@@ -16,6 +11,9 @@
             icon-right="keyboard_arrow_down"
           />
           <q-btn flat label="Register" class="q-mr-sm" />
+        </div>
+        <div class="gt-sm">
+          <q-btn push color="white" text-color="primary" label="Contact Us" />
         </div>
         <div class="lt-md">
           <q-btn
@@ -46,6 +44,19 @@
   </q-layout>
 </template>
 
+<style scoped>
+.custom-toolbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.gt-sm,
+.lt-md {
+  display: flex;
+  align-items: center;
+}
+</style>
+
 <script setup>
 import { ref } from "vue";
 import EssentialLink from "components/EssentialLink.vue";
@@ -65,6 +76,12 @@ const linksList = [
     title: "Sign Up",
     caption: "Register User",
     icon: "person_add",
+    link: "#",
+  },
+  {
+    title: "Contact Us",
+    caption: "Contact Hyperhire",
+    icon: "mail",
     link: "#",
   },
 ];
